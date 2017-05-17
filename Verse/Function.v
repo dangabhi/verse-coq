1370bf25877a9c53b4ebb55f9b112ecb36fc14cc
Abhishek Dang <ahdang@iitk.ac.in>
2017-05-17 20:13:12 +0530

    an ugly working definition of 'makeb' added

--- Verse/Function.v
+++ Verse/Function.v
@@ -6,6 +6,7 @@ Require Import String.
 Require Import Coq.Sets.Ensembles.
 Require Import List.
 Import ListNotations.
+Require Import Basics.
 
 Set Implicit Arguments.
 
@@ -15,17 +16,18 @@ Fixpoint listSet {A : Type} (l : list A) : Ensemble A :=
   | a :: lt => Ensembles.Add _ (listSet lt) a
   end.
 
+Fixpoint fbv (v : varT) (l : list type) :=
+  match l with
+  | [] => block v
+  | t :: lt => v t -> fbv v lt
+  end.
+
+Definition fblock (l : list type) :=
+  forall (v : varT), fbv v l.
+
 (** #######################
 The archvar that is passed to Function has to be the one with a dummy stack provided. The Arch module type should construct out of it's register and stack parameters a full archvar and a dummy archvar
  *)
-
-Fixpoint fblock (l : list type) :=
-  forall (v : varT),
-  (fix fbt (l : list type) :=
-    match l with
-    | []      => block v
-    | t :: lt => v t -> fbt lt
-    end) l.
 
 Record Function (archvar : varT) := func
                     {
@@ -52,8 +54,23 @@ Record Function (archvar : varT) := func
                       cleanup  : fblock (param ++ local);
                     }.
 
-Definition makeb {l : list type} (fb : fblock l) {v : varT} (alloc : {lv : list (sigT v) | map (@projT1 _ v) lv = l}) : block v.
+Lemma makebv {l : list type} {v : varT} (fb : fbv v l)  (alloc : {lv : list (sigT v) | map (@projT1 _ v) lv = l}) : block v.
+  induction l.
+  exact fb.
+  unfold fbv in fb.
+  destruct alloc.
+  induction x; simpl in e.
+  apply nil_cons in e. contradiction.
+  injection e; intros.
+  destruct a0.
+  simpl in H0.
+  assert (v0is : v a).
+  subst a. exact v0.
+  pose (fbn := fb v0is).
+  fold fbv in fbn.
+  exact (IHl fbn (exist _ x H)).
+Qed.
 
-(* ########################
-I am trying to write this yet.
-*)
+Definition makeb {l : list type} (fb : fblock l) {v : varT} (alloc : {lv : list (sigT v) | map (@projT1 _ v) lv = l}) : block v.
+exact (makebv (fb v) alloc).
+Qed.
