2b0a180692fbb27817d6ef57d51680d08ebf15d7
dangabhi <ahdang@iitk.ac.in>
2018-03-09 09:49:41 +0530

    simple CURSOR added to semantics

--- src/Verse/Semantics.v
+++ src/Verse/Semantics.v
@@ -4,13 +4,18 @@ Require Import Verse.Types.Internal.
 Require Import Verse.Syntax.
 Require Import Verse.DecFacts.
 Require Import Verse.Error.
+Require Import DecFacts.
 
 Require Import Vector.
 
 Set Implicit Arguments.
 
+Module Type CURSOR.
+  Parameter cursor : Type.
+  Parameter update : cursor -> cursor.
+End CURSOR.
 
-Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS W).
+Module Semantics (C : CURSOR) (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS W).
 
   Module T  := TypeDenote W.
   Module C  := ConstDenote W CW.
@@ -25,32 +30,39 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
 
     (* Variable errors *)
     Inductive VariableError : Prop :=
-    | InvalidatedAt : forall {k} (ty : type k), v ty -> instruction v -> VariableError
-    | OperationError : O.OpError -> instruction v -> VariableError.
+    | InvalidatedAt : forall {k} (ty : type k), v ty -> C.cursor -> instruction v -> VariableError
+    | OperationError : O.OpError -> C.cursor -> instruction v -> VariableError.
 
-    Definition State := forall k (ty : type k), v ty -> T.typeDenote ty + {VariableError}.
+    Record State :=
+      {   crsr : C.cursor;
+          env    : forall k (ty : type k), v ty -> T.typeDenote ty + {VariableError}
+      }.
 
     Inductive StateError : Prop :=
-    | InvalidUse : forall {k} {ty : type k}, v ty -> instruction v -> instruction v -> StateError
-    | OperatorError : O.OpError -> instruction v -> instruction v -> StateError.
+    | InvalidUse : forall {k} {ty : type k}, v ty -> C.cursor -> instruction v -> C.cursor -> instruction v -> StateError
+    | OperatorError : O.OpError -> C.cursor -> instruction v -> C.cursor -> instruction v -> StateError.
 
     (* Extracts from Stat the value denoted by an arg *)
     Definition argDenote (S : State) {k} {ty : type k} {aK} (a : arg v aK _ ty) : T.typeDenote ty + {VariableError} :=
+      let '{| crsr := _; env := Se |} := S in
       match a in (arg _ _ _ ty) return T.typeDenote ty + {VariableError} with
-      | var av           => S _ _ av
+      | var av               => Se _ _ av
       | Language.Ast.const c => {- C.constDenote c -}
-      | index x i        => (fun y => nth_order y (proj2_sig i)) <$> S _ _ x
+      | index x i            => (fun y => nth_order y (proj2_sig i)) <$> Se _ _ x
       end.
 
     (* The pattern match for stateUpdate is too gory to be illuminating *)
     Definition stateUpdate k {ty : type k} (var : v ty) (f : T.typeDenote ty + {VariableError} -> T.typeDenote ty + {VariableError}) :
       State -> State.
-      unfold State in *; intros; intros.
+      intro S; destruct S as [c e].
+      constructor.
+      exact c.
+      intros.
       destruct (kind_eq_dec k k0); subst.
       destruct (ty_eq_dec ty ty0); subst.
-      destruct (v_eq_dec X0 var); subst.
-      exact (f (X _ _ var)).               (* update according to f at var *)
-      all: exact (X _ _ X0).               (* use initial state value at all other points *)
+      destruct (v_eq_dec X var); subst.
+      exact (f (e _ _ var)).               (* update according to f at var *)
+      all: exact (e _ _ X).               (* use initial state value at all other points *)
     Defined.
 
     (* Auxiliary function to lift an arg value change to State *)
@@ -66,21 +78,22 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
       end val.
 
     Definition instructionDenote (i : instruction v) (S : State) : State + {StateError} :=
+      let '{| crsr := SC; env := SE |} := S in
       let pushErr Err T (p : T * T + {Err}) := match p with
                                               | {- (a, b) -} => ({- a -}, {- b -})
                                               | error e      => (error e, error e)
                                               end in
       let liftOpErr {T} (v : T + {O.OpError}) := match v with
                                                  | {- v -} => {- v -}
-                                                 | error e => error (OperationError e i)
+                                                 | error e => error (OperationError e SC i)
                                                  end in
       (* Auxiliary function to update arg values only when Valid *)
       let validate {k} {ty : type k} (a : larg v _ ty) val S :=
           match val with
           | {- oval -} => {- largUpdate a (liftOpErr oval) S -}
           | error e => error (match e with
-                              | InvalidatedAt v iAt => InvalidUse v iAt i
-                              | OperationError oe iAt => OperatorError oe iAt i
+                              | InvalidatedAt v c iAt => InvalidUse v c iAt SC i
+                              | OperationError oe c iAt => OperatorError oe c iAt SC i
                               end)
           end in
       let validatePair {k} {ty : type k} (a1 a2 : larg v _ ty) val :=
@@ -114,15 +127,42 @@ Module Semantics (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS
                                                           S
                       end
       | moveTo x ix ra => largUpdate (var ra)
-                                     (error (InvalidatedAt ra i))
+                                     (error (InvalidatedAt ra SC i))
                                      <$>
                                      validate (index x ix) (inleft <$> (@argDenote S _ _ rval (var ra)))
                                      S
       | CLOBBER ra     => {- largUpdate (var ra)
-                                        (error (InvalidatedAt ra i))
+                                        (error (InvalidatedAt ra SC i))
                                         S -}
       end.
 
   End InstructionDenote.
 
 End Semantics.
+
+Module PC <: CURSOR.
+  Definition cursor := nat. (* program counter *)
+  Definition update := S.
+End PC.
+
+Module PCSemantics := Semantics PC.
+
+Module CodeSemantics (C : CURSOR) (W : WORD_SEMANTICS) (CW : CONST_SEMANTICS W) (O : OP_SEMANTICS W).
+
+  Module S := Semantics C W CW O.
+  Import S.
+
+  Section CodeDenote.
+
+    Variable v : VariableT.
+    Variable v_eq_dec : forall {k} {ty : type k} (v1 v2 : v ty), {v1 = v2} + {v1 <> v2}.
+    Variable init : State v.
+
+    Definition codeDenote c := List.fold_right
+                                 (fun i s => bind s (instructionDenote v_eq_dec i))
+                                 {- init -}
+                                 c.
+  End CodeDenote.
+End CodeSemantics.
+
+Module PCCodeSemantics := CodeSemantics PC.
